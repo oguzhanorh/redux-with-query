d4f21d9a15eb339029f7cb4c15b67b56d84baa48
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-08-13 17:11:44 +0300

    add some properties with useSelector

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,14 +1,24 @@
 import { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 function UserList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return 'User List';
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+  return <div>{data.length}</div>;
 }
 export default UserList;
