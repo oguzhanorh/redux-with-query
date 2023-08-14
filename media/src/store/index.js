d3f92a9fc022691b74f290f6c9f2457d370a239a
Oğuzhan Orhan <oorhan@lstyazilim.com>
2023-08-14 16:28:59 +0300

    update slice

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -8,3 +8,4 @@ export const store = configureStore({
 });
 export * from './thunks/fetchUsers'; //merkezden dışara aktarmak için kullanıyoruz.
 export * from './thunks/addUser';
+export * from './thunks/removeUser';
