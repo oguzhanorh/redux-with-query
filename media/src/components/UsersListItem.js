3d9113ea484b8b7267fd31b10d3d3250e0ccbf7d
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-08-14 21:44:41 +0300

    CREATE expandable

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -2,29 +2,25 @@ import { BsTrash3 } from 'react-icons/bs';
 import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/use-thunk';
-
+import ExpandablePanel from './ExpandablePanel';
 
 function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
-    const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
 
-    const handleClick = () => {
-        doRemoveUser(user);
-    }
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <BsTrash3 />
+      </Button>
+      {error && <div>Error deleting user.</div>}
+      {user.name}
+    </>
+  );
 
-    return (
-        <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                <div className='flex flex-row items-center justify-between'>
-                    <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-                        <BsTrash3 />
-                    </Button>
-                    {error && <div>Error deleting user.</div>}
-                    {user.name}
-                </div>
-
-            </div>
-        </div>
-    );
+  return <ExpandablePanel header={header}>CONTENT !!!</ExpandablePanel>;
 }
-export default UsersListItem
+export default UsersListItem;
