f921c78797108ec4a4377e7faa0dba27806b8553
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-08-20 15:08:38 +0300

    show photo list

--- media/src/components/PhotosListItem.js
+++ media/src/components/PhotosListItem.js
@@ -1,4 +1,8 @@
-function PhotosListItem() {
-  return 'PhotosListItem';
+function PhotosListItem({ photo }) {
+  return (
+    <div>
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
+    </div>
+  );
 }
 export default PhotosListItem;
