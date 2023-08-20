452d8e04f0a6db521f71b4e552f2c04ddcd26fc9
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-08-20 15:14:39 +0300

    delete photo with hover

--- media/src/components/PhotosListItem.js
+++ media/src/components/PhotosListItem.js
@@ -1,7 +1,19 @@
+import { useRemovePhotoMutation } from '../store';
+import { BsTrash3 } from 'react-icons/bs';
+
 function PhotosListItem({ photo }) {
+  const [removePhoto, results] = useRemovePhotoMutation();
+
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
+
   return (
-    <div>
+    <div onClick={handleRemovePhoto} className="relative m-2 cursor-pointer">
       <img className="h-20 w-20" src={photo.url} alt="random pic" />
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <BsTrash3 className="text-3xl" />
+      </div>
     </div>
   );
 }
